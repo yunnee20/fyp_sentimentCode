e120d132ecae02658a9af7665b209f698dd48f0d
Yun Nee <y.tey0620241@arts.ac.uk>
2026-05-08 16:16:16 +0100

    testing face

--- states.py
+++ states.py
@@ -1,8 +1,11 @@
-from face import face_trigger
+from face import start_face_stream, stop_face_stream, get_face_score
 from textsentiment import analyze_text_sentiment
 from score import calculate_final_score
 from voicetotext import voice_to_text, check_ready
-from voice_freqpitch import start_voice_stream, stop_voice_stream
+from voice_freqpitch import start_voice_stream, stop_voice_stream, get_voice_score
+
+from face import start_face_stream, stop_face_stream, get_face_score
+
 
 ready = False
 startTest = False
@@ -43,7 +46,49 @@ def Ready():
 
 #     return answer_text
 
+
+
 def run_question(question_number):
+    print(f"Question {question_number} started")
+
+    # start real-time data to TouchDesigner
+    start_face_stream()
+    start_voice_stream()
+
+    # record + transcribe while streams are running
+    transcript = voice_to_text(duration=10)
+
+    # stop real-time streams
+    stop_face_stream()
+    stop_voice_stream()
+
+    # get average scores collected during answer
+    face_score = get_face_score()
+    voice_score = get_voice_score()
+
+    # text score after transcription
+    text_result = analyze_text_sentiment(transcript)
+    text_score = text_result["text_score"]
+
+    question_score = calculate_final_score(
+        face_score=face_score,
+        voice_score=voice_score,
+        text_score=text_score
+    )
+
+    question_result = {
+        "question_number": question_number,
+        "transcript": transcript,
+        "face_score": face_score,
+        "voice_score": voice_score,
+        **text_result,
+        **question_score
+    }
+
+    print("------ QUESTION RESULT ------")
+    print(question_result)
+
+    return question_result
     print(f"Question {question_number} started")
     start_voice_stream()
     # 1. Capture face snapshot
